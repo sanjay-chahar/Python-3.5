b19534b2550f6f9a955fa22450ff88ec0f9434c1
Sanjay Chahar <chaharsan@yahoo.com>
2017-08-17 16:31:27 +0100

    Add files via upload

--- funcation.py
+++ funcation.py
@@ -16,21 +16,20 @@
 #       print(name +  message)
 #       return
 # main()
+
 #Challeneg
 
 def main():
     messages = input('Please add your message : - ')
-    path = input('please enter full file name and path :- ')
-    with open(path, 'a') as filename:
-        filename.write(messages + '\n')
-        filename.close()
-    rfile(path)
+    filename = input('please enter full file name and path :- ')
+    filename = open(filename, mode='w')
+    filename = filename.write(messages)
+    readfile(filename)
     return
 
-def rfile(path):
-    filename = open(path, 'r')
-    print(filename.read())
-    filename.close()
+def readfile(filename):
+    open(filename, mode='r')
+    read(filename)
+    filename()
     return
-
 main()
