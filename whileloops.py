c23165e9e421ed24118256009a40fc59ae3b9555
Sanjay Chahar <chaharsan@yahoo.com>
2017-08-22 21:44:52 +0100

    Update whileloops.py

--- whileloops.py
+++ whileloops.py
@@ -20,6 +20,15 @@ while counter < 4:
 # while num < 1000:
 #     print('file %d created ' % num)
 #     num = num + 1
-
+while True:
+  print('Who are you?')
+  name = input()
+  if name != 'Joe':
+   continue
+  print('Hello, Joe. What is the password? (It is a fish.)')
+  password = input()
+  if password == 'swordfish':
+    break
+print('Access granted.')
 
 
